aeb9f907bf7cb629678e16179614db7bc5c3b501
cent-dxv <centdxv@gmail.com>
2023-12-18 20:15:43 +0300

    auto_src/app/page.js

---
+++ src/app/page.js
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+import style_navbar from '@/styles/components/navbar.module.scss'
+import Course_Main from '../components/courses_page/main'
+
+export default function Home() {
+  return (
+    <>
+      <div className={``}>
+      <Course_Main/>
+
+      </div>
+
+
+    </>
+  )
+}
