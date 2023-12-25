8811a7b0d9847fed846ec2c1be9acf6e7e1fdb8f
cent-dxv <centdxv@gmail.com>
2023-12-25 15:35:30 +0300

    auto_mok_data.js

---
+++ mok_data.js
@@ -0,0 +1,174 @@
+
+const result_data = [
+
+
+  {
+    "id": 1,
+    "image_url": "https://images.pexels.com/photos/4491461/pexels-photo-4491461.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "title": "1 רטסמס הקיטמתמ תניכמ",
+    "subtitle": "ןושאר",
+    "lectureure": "ןהכ זרא",
+    "lecturer_img": "https://xsgames.co/randomusers/avatar.php?g=male",
+    "rates": "4.8",
+    "price": "185.00",
+
+  },
+  {
+    "id": 2,
+    "image_url": "https://images.unsplash.com/photo-1448932223592-d1fc686e76ea?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fG9ubGluZSUyMGNvdXJzZXxlbnwwfHwwfHx8MA%3D%3D",
+    "title": "2 רטסמס הקיטמתמ תניכמ",
+    "subtitle": "ןושאר",
+    "lectureure": "ןהכ זרא",
+    "lecturer_img": "https://xsgames.co/randomusers/avatar.php?g=male",
+    "rates": "4.8",
+    "price": "185.00",
+
+  }, {
+    "id": 3,
+    "image_url": "https://plus.unsplash.com/premium_photo-1661670262286-d6a32dbb3c78?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "title": "3 רטסמס הקיטמתמ תניכמ",
+    "subtitle": "ןושאר",
+    "lectureure": "ןהכ זרא",
+    "lecturer_img": "https://xsgames.co/randomusers/avatar.php?g=male",
+    "rates": "4.8",
+    "price": "185.00",
+
+  }, {
+    "id": 4,
+    "image_url": "https://images.pexels.com/photos/4497761/pexels-photo-4497761.jpeg",
+    "title": "4 רטסמס הקיטמתמ תניכמ",
+    "subtitle": "ןושאר",
+    "lectureure": "ןהכ זרא",
+    "lecturer_img": "https://xsgames.co/randomusers/avatar.php?g=male",
+    "rates": "4.8",
+    "price": "185.00",
+
+  }, {
+    "id": 5,
+    "image_url": "https://images.pexels.com/photos/5466193/pexels-photo-5466193.jpeg",
+    "title": "5 רטסמס הקיטמתמ תניכמ",
+    "subtitle": "ןושאר",
+    "lectureure": "ןהכ זרא",
+    "lecturer_img": "https://xsgames.co/randomusers/avatar.php?g=male",
+    "rates": "4.8",
+    "price": "185.00",
+
+  }, {
+    "id": 6,
+    "image_url": "https://images.unsplash.com/photo-1522202176988-66273c2fd55f?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "title": "רטסמס הקיטמתמ תניכמ",
+    "subtitle": "ןושאר",
+    "lectureure": "ןהכ זרא",
+    "lecturer_img": "https://xsgames.co/randomusers/avatar.php?g=male",
+    "rates": "4.8",
+    "price": "185.00",
+
+  }, {
+    "id": 7,
+    "image_url": "https://images.pexels.com/photos/4492160/pexels-photo-4492160.jpeg",
+    "title": "רטסמס הקיטמתמ תניכמ",
+    "subtitle": "ןושאר",
+    "lectureure": "ןהכ זרא",
+    "lecturer_img": "https://xsgames.co/randomusers/avatar.php?g=male",
+    "rates": "4.8",
+    "price": "185.00",
+
+  }, {
+    "id": 8,
+    "image_url": "https://images.pexels.com/photos/6929206/pexels-photo-6929206.jpeg",
+    "title": "רטסמס הקיטמתמ תניכמ",
+    "subtitle": "ןושאר",
+    "lectureure": "ןהכ זרא",
+    "lecturer_img": "https://xsgames.co/randomusers/avatar.php?g=male",
+    "rates": "4.8",
+    "price": "185.00",
+
+  }, {
+    "id": 9,
+    "image_url": "https://images.pexels.com/photos/920382/pexels-photo-920382.jpeg",
+    "title": "רטסמס הקיטמתמ תניכמ",
+    "subtitle": "ןושאר",
+    "lectureure": "ןהכ זרא",
+    "lecturer_img": "https://xsgames.co/randomusers/avatar.php?g=male",
+    "rates": "4.8",
+    "price": "185.00",
+
+  }, {
+    "id": 10,
+    "image_url": "https://images.pexels.com/photos/838413/pexels-photo-838413.jpeg",
+    "title": "רטסמס הקיטמתמ תניכמ",
+    "subtitle": "ןושאר",
+    "lectureure": "ןהכ זרא",
+    "lecturer_img": "https://xsgames.co/randomusers/avatar.php?g=male",
+    "rates": "4.8",
+    "price": "185.00",
+
+  }, {
+    "id": 11,
+
+    "image_url": "https://images.pexels.com/photos/4623357/pexels-photo-4623357.jpeg",
+    "title": "רטסמס הקיטמתמ תניכמ",
+    "subtitle": "ןושאר",
+    "lectureure": "ןהכ זרא",
+    "lecturer_img": "https://xsgames.co/randomusers/avatar.php?g=male",
+    "rates": "4.8",
+    "price": "185.00",
+
+  }, {
+    "id": 12,
+    "image_url": "https://images.pexels.com/photos/6517343/pexels-photo-6517343.jpeg",
+    "title": "רטסמס הקיטמתמ תניכמ",
+    "subtitle": "ןושאר",
+    "lectureure": "ןהכ זרא",
+    "lecturer_img": "https://xsgames.co/randomusers/avatar.php?g=male",
+    "rates": "4.8",
+    "price": "185.00",
+
+  },
+  {
+    "id": 13,
+    "image_url": "https://images.pexels.com/photos/6281877/pexels-photo-6281877.jpeg",
+    "title": "רטסמס הקיטמתמ תניכמ",
+    "subtitle": "ןושאר",
+    "lectureure": "ןהכ זרא",
+    "lecturer_img": "https://xsgames.co/randomusers/avatar.php?g=male",
+    "rates": "4.8",
+    "price": "185.00",
+
+  },
+  {
+    "id": 14,
+    "image_url": "https://images.pexels.com/photos/5553045/pexels-photo-5553045.jpeg",
+    "title": "רטסמס הקיטמתמ תניכמ",
+    "subtitle": "ןושאר",
+    "lectureure": "ןהכ זרא",
+    "lecturer_img": "https://xsgames.co/randomusers/avatar.php?g=male",
+    "rates": "4.8",
+    "price": "185.00",
+
+  },
+  {
+    "id": 15,
+    "image_url": "https://images.pexels.com/photos/5905748/pexels-photo-5905748.jpeg",
+    "title": "רטסמס הקיטמתמ תניכמ",
+    "subtitle": "ןושאר",
+    "lectureure": "ןהכ זרא",
+    "lecturer_img": "https://xsgames.co/randomusers/avatar.php?g=male",
+    "rates": "4.8",
+    "price": "185.00",
+
+  },
+  {
+    "id": 16,
+    "image_url": "https://images.unsplash.com/photo-1664574654700-75f1c1fad74e?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "title": "רטסמס הקיטמתמ תניכמ",
+    "subtitle": "ןושאר",
+    "lectureure": "ןהכ זרא",
+    "lecturer_img": "https://xsgames.co/randomusers/avatar.php?g=male",
+    "rates": "4.8",
+    "price": "185.00",
+
+  },
+]
+
+export default result_data
