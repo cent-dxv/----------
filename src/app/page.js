e7a8b202cd68501fda234f7d1073127abfea3cc1
cent-dxv <centdxv@gmail.com>
2023-12-25 14:37:23 +0300

    auto_src/app/page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import style_navbar from '@/styles/components/navbar.module.scss'
 import Course_Main from '../components/courses_page/main'
+import Footer from '../components/footer'
 
 export default function Home() {
   return (
@@ -8,7 +9,9 @@ export default function Home() {
       <div className={``}>
       <Course_Main/>
 
+
       </div>
+      <Footer/>
 
 
     </>
