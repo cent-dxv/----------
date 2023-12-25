2b1939644ee4e68259a17bb34c759038ced43a53
cent-dxv <centdxv@gmail.com>
2023-12-25 14:37:15 +0300

    auto_src/app/home/layout.js

---
+++ src/app/home/layout.js
@@ -0,0 +1,13 @@
+import Footer from "@/src/components/footer";
+
+export default function DashboardLayout({
+  children, // will be a page or nested layout
+}) {
+  return (
+    <section>
+      
+ 
+      {children}
+    </section>
+  )
+}
